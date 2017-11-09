374de608ef1a307608dc51526c9773c4da1983f3
Carlos Cruz Domínguez <carloscruzdominguez97@gmail.com>
2017-11-09 14:30:18 +0100

    Subo

--- app/src/main/java/com/example/red/AsincronaActivity.java
+++ app/src/main/java/com/example/red/AsincronaActivity.java
@@ -3,6 +3,11 @@ package com.example.red;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * Lo que deberia contener esta clase lo he hecho en HTTPActivity, ya que me he equivocado, pero todavia
+ * tiene el codigo comentado de la manera no asíncrona de acceder a descargar datos de un servidor web.
+ */
 public class AsincronaActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/red/HTTPActivity.java
+++ app/src/main/java/com/example/red/HTTPActivity.java
@@ -10,6 +10,9 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
 
 public class HTTPActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -19,6 +22,10 @@ public class HTTPActivity extends AppCompatActivity implements View.OnClickListe
     private RadioButton radioApache;
     private Button btnConectar;
     private TextView tiempo;
+
+    private long tiempoInicio;
+    private long tiempoFin;
+    private boolean radioJavaState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +43,7 @@ public class HTTPActivity extends AppCompatActivity implements View.OnClickListe
         btnConectar.setOnClickListener(this);
         web = (WebView) findViewById(R.id.wbvContenido);
         tiempo = (TextView) findViewById(R.id.txvTiempo);
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        //StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
     }
 
 
@@ -47,6 +54,7 @@ public class HTTPActivity extends AppCompatActivity implements View.OnClickListe
         Resultado resultado;
         switch (view.getId()) {
             case R.id.btnConectar:
+                radioJavaState = radioJava.isChecked();
                 Toast.makeText(this, "HOLA SOY EL BOTON", Toast.LENGTH_LONG).show();
                 establecerConexion(texto);
                 break;
@@ -54,7 +62,7 @@ public class HTTPActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void establecerConexion(String texto) {
-        long inicio;
+        /*long inicio;
         Resultado resultado;
         long fin;
         inicio = System.currentTimeMillis();
@@ -67,6 +75,65 @@ public class HTTPActivity extends AppCompatActivity implements View.OnClickListe
             web.loadDataWithBaseURL(null, resultado.getContenido(),"text/html", "UTF-8", null);
         else
             web.loadDataWithBaseURL(null, resultado.getMensaje(),"text/html", "UTF-8", null);
-        tiempo.setText("Duración: " + String.valueOf(fin - inicio) + " milisegundos");
+        tiempo.setText("Duración: " + String.valueOf(fin - inicio) + " milisegundos");*/
+        tiempoInicio = System.currentTimeMillis();
+        new TareaAsincrona().execute(new String[] {texto});
+    }
+
+    public class TareaAsincrona extends AsyncTask<String, Void, Resultado> {
+        private ProgressDialog progreso;
+
+        protected void onPreExecute() {
+            progreso = new ProgressDialog(HTTPActivity.this);
+            progreso.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progreso.setMessage("Conectando . . .");
+            progreso.setCancelable(false);
+            progreso.show();
+        }
+
+        //Aqui realizamos las tareas. No debemos acceder a los elementos, vistas desde aqui, como mucho en el onPreExecute y en
+        //el onPostExecute. Si necesitamos aqui datos externos de elementos como el estado del radio button de java,
+        //lo guardamos en antes de llamar a la tarea asincrona en una variable global. Hago lo mismo para los tiempos de
+        //inicio de descarga de los datos del servidor web y de fin
+        //para poder guardarlos desde cualquier lado y accederlos desde cualquier lado.
+        protected Resultado doInBackground(String... cadena) {
+            Resultado resultado;
+            try {
+                if (radioJavaState) {
+                    resultado = Conectar.conectarJava(cadena[0]);
+                } else {
+                    resultado = Conectar.conectarApache(cadena[0]);
+                }
+            } catch (Exception e) {
+                Log.e("HTTP", e.getMessage(), e);
+                resultado = null;
+                cancel(true);
+            }
+            return resultado;
+        }
+
+
+
+        //Debemos acceder a los elementos, a las vistas, desde el onPostExecute.
+        protected void onPostExecute(Resultado result) {
+            tiempoFin = System.currentTimeMillis();
+            progreso.setMessage("Ha terminado la ejecución");
+            progreso.dismiss();
+            if (result.getCodigo())
+                web.loadDataWithBaseURL(null, result.getContenido(),"text/html", "UTF-8", null);
+            else
+                web.loadDataWithBaseURL(null, result.getMensaje(),"text/html", "UTF-8", null);
+            tiempo.setText("Duración: " + String.valueOf(tiempoFin - tiempoInicio) + " milisegundos");
+        }
+
+
+
+        protected void onCancelled() {
+            progreso.setMessage("Se ha cancelado la ejecución");
+            progreso.dismiss();
+            // mostrar cancelación
+        }
     }
 }
+
+
